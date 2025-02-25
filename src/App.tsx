359d94be76cb9ca52d5b04a802dbeae242494703
sanjibroy360 <sanjibroy0098@gmail.com>
2025-02-25 12:57:41 +0530

    chore: initialize React 19 app with Vite and set up TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-center bg-blue-50 min-h-screen font-bold underline">
+      React 19 with Tailwind CSS
+    </h1>
+  );
+}
+
+export default App;
